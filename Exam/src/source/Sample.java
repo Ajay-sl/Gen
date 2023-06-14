bffb1c462a557f320c25737965c0d02fe7555e49
Ajay <ajay03m12@gmail.com>
2023-06-14 16:18:30 +0530

    Intial commit

---
+++ Exam/src/source/Sample.java
@@ -0,0 +1,21 @@
+package source;
+
+public class Sample{
+	public static void main(String[] args) {
+		int a=10;
+		
+		String b=Integer.toString(a);
+		String c=String.valueOf(a);
+		
+		String d="120";
+		int e=Integer.parseInt(d);
+		System.out.println(e);
+		
+		
+	}
+	
+	}
+
+
+
+
